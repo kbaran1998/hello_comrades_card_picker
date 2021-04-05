b0461ed0c8b66c32afa8f12e6ba747deb84ea83c
Krzysztof <krysbaran@gmail.com>
2021-04-05 18:11:53 +0200

    Other files

--- file_converter.py
+++ file_converter.py
@@ -1,28 +1,56 @@
 import csv
 import json
 
-final_set = []
 
-with open('CARDS.csv', encoding="utf8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        card_dict = dict()
-        card_dict['description'] = row[0]
-        card_dict['options'] = []
-        for i in range(1, 5):
-            if not row[i]:
-                break
-            consequences_dict = {
-                'reputation': row[2+3*i],
-                'money': row[2+3*i+1],
-                'health': row[2+3*i+2]
-            }
-            option_dict = {'name': row[i], 'consequences': consequences_dict}
-            card_dict['options'].append(option_dict)
-        final_set.append(card_dict)
+def convert_red_cards():
+    final_set = []
 
-json_object = json.dumps(final_set, indent=4)
-print(json_object)
+    with open('RED_CARDS.csv', encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            card_dict = dict()
+            card_dict['description'] = row[0].replace(";", ",")
+            card_dict['options'] = []
+            for i in range(1, 4):
+                if not row[i]:
+                    break
+                consequences_dict = {
+                    'reputation': row[1+3*i],
+                    'money': row[1+3*i+1],
+                    'health': row[1+3*i+2]
+                }
+                option_dict = {'name': row[i],
+                               'consequences': consequences_dict}
+                card_dict['options'].append(option_dict)
+            final_set.append(card_dict)
 
-with open("red_cards.json", "w") as outfile:
-    outfile.write(json_object)
+    json_object = json.dumps(final_set, indent=4)
+    print(json_object)
+    with open("red_cards.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+def convert_gold_cards():
+    final_set = []
+    with open('CARDS_GOLD.csv', encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            card_dict = dict()
+            card_dict['description'] = row[0].replace(";", ",")
+            amount_type_arr = row[1].split()
+            if amount_type_arr[1] == "Money":
+                amount_type_arr[1] = "money"
+            elif amount_type_arr[1] == "REP":
+                amount_type_arr[1] = "reputation"
+            else:
+                amount_type_arr[1] = "health"
+            card_dict['reward'] = {
+                "amount": amount_type_arr[0], "type": amount_type_arr[1]}
+            final_set.append(card_dict)
+
+    json_object = json.dumps(final_set, indent=4)
+    with open("gold_cards.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+convert_gold_cards()
--- main.py
+++ main.py
@@ -9,33 +9,11 @@ def read_cards():
     return red_cards
 
 
-def card_menu(card):
-    print()
-    input_int = ''
-    while input_int != 'q':
-        print(card['description'])
-        for i in range(len(card['options'])):
-            print("         ", i+1, "-", card['options'][i]['name'])
-        try:
-            input_int = int(input('Make your choice: '))
-            if 1 <= input_int <= len(card['options']):
-                print(card['options'][i]['consequences'])
-                input_int = 'q'
-            else:
-                print(f"Pick number beteween 1 - {len(card['options'])} !!!")
-        except Exception:
-            print("Your instruction is incorrect!")
-        print()
-
-
-cards = read_cards()
 input_str = 's'
 while input_str != 'q':
     if len(cards) == 0:
         cards = read_cards()
     try:
         input_str = input("card-picker> ")
-        if input_str == 'pick':
-            card_menu(cards.pop())
     except Exception:
         print("Your instruction is incorrect!")
